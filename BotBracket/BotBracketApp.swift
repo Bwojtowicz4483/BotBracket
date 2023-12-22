a6969a1025e23b947fcbc0d2262bf18cb3c33911
Ben Wojtowicz <Bwojtowicz4483@stu.d214.org>
2023-12-22 08:52:39 -0600

    Initial Commit

---
+++ BotBracket/BotBracketApp.swift
@@ -0,0 +1,17 @@
+//
+//  BotBracketApp.swift
+//  BotBracket
+//
+//  Created by Ben Wojtowicz on 12/22/23.
+//
+
+import SwiftUI
+
+@main
+struct BotBracketApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
