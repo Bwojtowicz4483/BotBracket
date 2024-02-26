03f741631e806ce2c15244d93e6b77d1c9e75731
VinceVent <vchen4070@stu.d214.org>
2024-02-26 11:04:18 -0600

    aboutview half done just need video

--- BotBracket/AboutView.swift
+++ BotBracket/AboutView.swift
@@ -13,6 +13,45 @@ struct AboutView: View {
     var body: some View {
         VStack{
             HeaderView()
+
+            Divider()
+            Text("Meet the Developer")
+                
+                .font(.largeTitle)
+            Divider()
+          
+            Text("Version 1.0")
+                .frame(width: 250)
+               
+            Divider()
+            VStack{
+                Text("Andrew Nowak")
+                Text("Adam Cubas")
+                Text("Ashley Saju")
+            }
+            Divider()
+          
+            Text("Version 2.0")
+                .frame(width: 250)
+                
+            Divider()
+            VStack{
+                Text("Ben Wojtowicz")
+                Text("Cody Brown")
+                Text("Vincent Chen")
+            }
+            Divider()
+         Text("Learn What We Do")
+             
+                .font(.largeTitle)
+            Divider()
+            VStack{
+                
+                
+            }
+            
+            
+            
         }
     }
 }
