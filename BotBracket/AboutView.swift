bc0eaad55cba09a00e36c253dd631568c2dd8cc2
VinceVent <vchen4070@stu.d214.org>
2024-03-07 11:16:53 -0600

    the scroll is done

--- BotBracket/AboutView.swift
+++ BotBracket/AboutView.swift
@@ -12,46 +12,49 @@ import WebKit
 struct AboutView: View {
     
     var body: some View {
-        VStack{
-            HeaderView()
-
-            Divider()
-            Text("Meet the Developer")
-                
-                .font(.largeTitle)
-            Divider()
-          
-            Text("Version 1.0")
-                .frame(width: 250)
-               
-            Divider()
+        
             VStack{
-                Text("Andrew Nowak")
-                Text("Adam Cubas")
-                Text("Ashley Saju")
-            }
-            Divider()
-          
-            Text("Version 2.0")
-                .frame(width: 250)
+                HeaderView()
+                ScrollView(.vertical, showsIndicators: false){
+                Divider()
+                Text("Meet the Developer")
                 
-            Divider()
-            VStack{
-                Text("Ben Wojtowicz")
-                Text("Cody Brown")
-                Text("Vincent Chen")
-            }
-            Divider()
-         Text("Learn What We Do")
-             
-                .font(.largeTitle)
-            Divider()
-            VStack{
+                    .font(.largeTitle)
+                Divider()
                 
+                Text("Version 1.0")
+                    .frame(width: 250)
                 
+                Divider()
+                VStack{
+                    Text("Andrew Nowak")
+                    Text("Adam Cubas")
+                    Text("Ashley Saju")
+                }
+                Divider()
+                
+                Text("Version 2.0")
+                    .frame(width: 250)
+                
+                Divider()
+                VStack{
+                    Text("Ben Wojtowicz")
+                    Text("Cody Brown")
+                    Text("Vincent Chen")
+                }
+                Divider()
+                Text("Learn What We Do")
+                
+                    .font(.largeTitle)
+                Divider()
+                VStack{
+                    
+                    
+                }
             }
             
             
         }
+        
     }
 }
